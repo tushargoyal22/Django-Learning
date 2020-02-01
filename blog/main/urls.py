aa05ad6a9f23052d8b60fed19ab4a1ffdb07d038
b18030 <tushar22.tg.tg@gmail.com>
2020-02-01 23:44:47 +0530

    configure for POST Requests

--- blog/main/urls.py
+++ blog/main/urls.py
@@ -17,7 +17,10 @@ from django.urls import path,include
 from main import views
 
 urlpatterns = [
-    path('',views.index)
+    path('',views.index),
+    path('article/<int:pk>',views.article,name='get_article'),
+    path('author/<int:pk>' ,views.author,name='get_author'),
+    path('article' ,views.create_article)
 ]
 
 
--- blog/main/views.py
+++ blog/main/views.py
@@ -1,5 +1,6 @@
 
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render,get_object_or_404
 from main import models
 
 # Create your views here.
@@ -20,10 +21,47 @@ def index(request):
 
 
 def article(request,pk):
-    article=models.Article.objects.get(pk=pk)
+    # article=models.Article.objects.get(pk=pk)
+    article= get_object_or_404(models.Article,pk=pk)
 
     context={
-        'articel':article
+        'article':article
     }
 
     return render(request,'main/article.html',context)
+
+
+def author(request,pk):
+
+    author=get_object_or_404(models.Author , pk=pk)
+    context={
+        "author":author
+    }
+
+    return render(request,'main/author.html',context)
+
+
+def create_article(request):
+    # print(request.POST)
+
+    authors=models.Author.objects.all()
+    context = {
+        "authors":authors
+    }
+
+    if(request.method=="POST"):
+        article_data={
+            "title":request.POST['title'],
+            "content":request.POST['content']
+        }
+
+        article=models.Article.objects.create(**article_data)
+        # author = models.Author.objects.get(pk= request.POST['author'])
+        # article.authors.set([author])
+        
+        author = models.Author.objects.filter(pk= request.POST['author'])
+        article.authors.set(author)
+
+
+        context["success"] = True
+    return render(request,'main/create_article.html',context)
